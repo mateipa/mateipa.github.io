7ef3dc210ea96b84e429616e4c5cdd18ae955066
Mario Teixeira Parente <mario.parente@gmx.de>
2021-10-27 21:53:43 +0200

    Change to imshow

--- assets/images/fpr-preval-ppv.py
+++ assets/images/fpr-preval-ppv.py
@@ -4,18 +4,25 @@ import seaborn as sns
 
 sns.set()
 sns.set_style('whitegrid')
-sns.set_context('talk')
+sns.set_context('paper', font_scale=1.5)
 
 
-fpr_v, preval_v = np.meshgrid(np.linspace(0, 0.1, num=100), np.logspace(-4, 0, num=100))
+fpr = np.linspace(0, 0.1, num=100)
+preval = np.logspace(-4, 0, num=100)
+fpr_v, preval_v = np.meshgrid(fpr, preval)
 
 ppv_v = 1 / (1 - fpr_v + fpr_v/preval_v)
 
 plt.figure(figsize=(6, 4))
-plt.contourf(fpr_v, preval_v, ppv_v, levels=np.linspace(0, 1, num=6))
-plt.yscale('log')
+ax = plt.gca()
+plt.imshow(ppv_v, extent=[np.min(fpr), np.max(fpr),
+                          np.min(preval), np.max(preval)],
+           aspect='auto', origin='lower', vmin=0, vmax=1)
+plt.yticks(np.linspace(0, 1, num=5))
+ax.set_yticklabels([f'$10^{{{exp}}}$' for exp in range(-4, 1)])
 plt.xlabel('FPR')
 plt.ylabel('Prevalence')
+plt.grid(False)
 plt.colorbar()
 plt.tight_layout()
 
